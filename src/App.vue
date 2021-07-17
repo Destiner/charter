aaa2c6ce062dabe7f349f463060f18080a1980ba
Timur Badretdinov <destinerx@gmail.com>
2021-07-17 14:51:22 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<div>
+			<router-link :to="'/'">
+				Home
+			</router-link>
+			<router-link :to="'/about'">
+				About
+			</router-link>
+		</div>
+		<router-view />
+	</div>
+</template>
+
+<style>
+body {
+	margin: 0;
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', Helvetica, Arial, sans-serif;
+}
+</style>
