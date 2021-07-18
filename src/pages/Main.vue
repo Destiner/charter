af814057fcc389759333b1ae28258a5e221597bf
Timur Badretdinov <destinerx@gmail.com>
2021-07-18 12:33:17 +0300

    Use native checkboxes instead of Switch

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -14,34 +14,21 @@
 				<h2>Chart Type</h2>
 				<div class="chart-types" />
 				<div class="chart-modifiers">
-					<div class="modifier">
-						<Switch
+					<div>
+						<input
 							v-model="isStacked"
-							:class="{ 'active': isStacked }"
-							class="switch"
+							type="checkbox"
+							name="stacked"
 						>
-							<span class="sr-only">Stacked</span>
-							<span
-								:class="{ 'active': isStacked }"
-								class="switch-body"
-							/>
-						</Switch>
-						Stacked
+						<label for="stacked">Stacked</label>
 					</div>
-
-					<div class="modifier">
-						<Switch
+					<div>
+						<input
 							v-model="isNormalized"
-							:class="{ 'active': isNormalized }"
-							class="switch"
+							type="checkbox"
+							name="normalized"
 						>
-							<span class="sr-only">Normalized</span>
-							<span
-								:class="{ 'active': isNormalized }"
-								class="switch-body"
-							/>
-						</Switch>
-						Normalized
+						<label for="normalized">Normalized</label>
 					</div>
 				</div>
 			</div>
@@ -56,13 +43,9 @@
 </template>
 
 <script lang="ts">
-import { Switch } from '@headlessui/vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-	components: {
-		Switch,
-	},
 	setup() {
 		const isStacked = ref(false);
 		const isNormalized = ref(false);
@@ -83,46 +66,5 @@ export default defineComponent({
 
 .input, .output {
 	flex: 1;
-}
-
-.switch {
-	display: inline-flex;
-	flex-shrink: 0;
-	width: 54px;
-	height: 28px;
-	padding: 0;
-	border: 2px outset transparent;
-	border-radius: 14px;
-	background: #0f766e;
-	cursor: pointer;
-}
-
-.switch.active {
-	background: #134e4a;
-}
-
-.switch-body {
-	display: inline-block;
-	width: 24px;
-	height: 24px;
-	border-radius: 50%;
-	background: white;
-	pointer-events: none;
-}
-
-.switch-body.active {
-	transform: translate(26px);
-}
-
-.sr-only {
-	position: absolute;
-	width: 1px;
-	height: 1px;
-	margin: -1px;
-	padding: 0;
-	overflow: hidden;
-	clip: rect(0,0,0,0);
-	border-width: 0;
-	white-space: nowrap;
 }
 </style>
