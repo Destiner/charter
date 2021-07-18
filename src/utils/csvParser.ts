933982dcc31e0198481ab057da390c3e47c6da91
Timur Badretdinov <destinerx@gmail.com>
2021-07-18 18:44:28 +0300

    Support timestamped CSV data

--- src/utils/csvParser.ts
+++ src/utils/csvParser.ts
@@ -1,11 +1,14 @@
-interface CSV {
-	header: string[];
+export interface CSV {
+	ids: string[];
 	values: number[][];
+	timestamps: number[];
 }
 
 export function parseCSV(input: string): CSV | undefined {
-	// Each row should have the same number of values
+	// Each row should have the same number of values, at least 2
 	// Each value should be a number (except for the first row)
+	// The first row of the dataset is header
+	// The first element of each row is a timestamp (except for the first row)
 	if (!input) {
 		return;
 	}
@@ -16,19 +19,29 @@ export function parseCSV(input: string): CSV | undefined {
 	if (!areColumnsBalanced) {
 		return;
 	}
-	const header = tokens[0];
+	const areAtLeastTwoColumns = columnCount[0] > 1;
+	if (!areAtLeastTwoColumns) {
+		return;
+	}
+	const ids = tokens[0].slice(1);
 	const valueTokens = tokens.slice(1);
 	const values = valueTokens.map((rowTokens) =>
-		rowTokens.map((token) => parseFloat(token)),
+		rowTokens.slice(1).map((token) => parseFloat(token)),
 	);
+	const timestamps = valueTokens.map((rowTokens) => parseInt(rowTokens[0]));
 	const areValuesValid = values.every((row) =>
 		row.every((value) => !isNaN(value)),
 	);
 	if (!areValuesValid) {
 		return;
 	}
+	const areTimestampsValid = timestamps.every((timestamp) => !isNaN(timestamp));
+	if (!areTimestampsValid) {
+		return;
+	}
 	return {
-		header,
+		ids,
 		values,
+		timestamps,
 	};
 }
