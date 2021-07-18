e9ddf1ec8046b0b0a5dfe6c79601b6d84c009098
Timur Badretdinov <destinerx@gmail.com>
2021-07-18 20:32:30 +0300

    Add todo list

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,14 @@ body {
 	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', Helvetica, Arial, sans-serif;
 }
 </style>
+
+<!--
+* fix normalized charts
+* datatype (numeric, currency, percentage)
+* chart bg
+* custom color schemes
+
+* multiple data types
+* export as svg/png
+* mobile ready
+-->
