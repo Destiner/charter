b6134f9807855242c5397d66d44467d24ae7ce80
Timur Badretdinov <destinerx@gmail.com>
2021-08-01 17:24:34 +0300

    Imagery for chart type options

---
+++ src/components/options/image/OptionImageType.vue
@@ -0,0 +1,195 @@
+<template>
+	<svg
+		v-if="value === 'area'"
+		viewBox="0 0 128 64"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+	>
+		<g
+			stroke="none"
+			stroke-width="1"
+			fill="none"
+			fill-rule="evenodd"
+		>
+			<g transform="translate(-226.000000, -268.000000)">
+				<g transform="translate(70.000000, 109.000000)">
+					<g transform="translate(0.000000, 105.000000)">
+						<g transform="translate(0.000000, 52.000000)">
+							<g transform="translate(154.000000, 0.000000)">
+								<g transform="translate(2.000000, 2.000000)">
+									<path d="M14,0 L114,0 C121.731986,-1.42034288e-15 128,6.2680135 128,14 L128,64 L128,64 L0,64 L0,14 C-9.46895252e-16,6.2680135 6.2680135,1.42034288e-15 14,0 Z" />
+									<path
+										d="M5.68434189e-14,41.7765736 C14.8046982,39.1402479 26.812419,37.822085 36.0231625,37.822085 C49.8392777,37.822085 72.8862525,47.0824765 92.3071841,48.2209772 C105.254472,48.9799777 117.152077,46.6892414 128,41.3487683 L128,64 L5.68434189e-14,64 L5.68434189e-14,41.7765736 Z"
+										fill="#BFDBFE"
+									/>
+									<path
+										d="M0,52.7367747 C6.55344514,51.9441571 21.0251398,50.105716 43.4150839,47.2214513 C65.805028,44.3371867 94,33.9300363 128,16 L128,64 L0,64 L0,52.7367747 Z"
+										fill="#93C5FD"
+									/>
+								</g>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+		</g>
+	</svg>
+	<svg
+		v-else-if="value === 'bar'"
+		viewBox="0 0 128 64"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+	>
+		<g
+			stroke="none"
+			stroke-width="1"
+			fill="none"
+			fill-rule="evenodd"
+		>
+			<g transform="translate(-380.000000, -268.000000)">
+				<g transform="translate(70.000000, 109.000000)">
+					<g transform="translate(0.000000, 105.000000)">
+						<g transform="translate(0.000000, 52.000000)">
+							<g transform="translate(308.000000, 0.000000)">
+								<g transform="translate(2.000000, 2.000000)">
+									<path d="M14,0 L114,0 C121.731986,-1.42034288e-15 128,6.2680135 128,14 L128,64 L128,64 L0,64 L0,14 C-9.46895252e-16,6.2680135 6.2680135,1.42034288e-15 14,0 Z"/>
+									<g transform="translate(12.000000, 41.000000)">
+										<rect
+											fill="#BFDBFE"
+											x="0"
+											y="0"
+											width="8"
+											height="23"
+										/>
+										<rect
+											fill="#93C5FD"
+											x="8"
+											y="10"
+											width="8"
+											height="13"
+										/>
+									</g>
+									<g transform="translate(34.000000, 38.000000)">
+										<rect
+											fill="#BFDBFE"
+											x="0"
+											y="0"
+											width="8"
+											height="26"
+										/>
+										<rect
+											fill="#93C5FD"
+											x="8"
+											y="9"
+											width="8"
+											height="17"
+										/>
+									</g>
+									<g transform="translate(56.000000, 41.000000)">
+										<rect
+											fill="#BFDBFE"
+											x="0"
+											y="1"
+											width="8"
+											height="22"
+										/>
+										<rect
+											fill="#93C5FD"
+											x="8"
+											y="0"
+											width="8"
+											height="23"
+										/>
+									</g>
+									<g transform="translate(78.000000, 31.000000)">
+										<rect
+											fill="#BFDBFE"
+											x="0"
+											y="16"
+											width="8"
+											height="17"
+										/>
+										<rect
+											fill="#93C5FD"
+											x="8"
+											y="0"
+											width="8"
+											height="33"
+										/>
+									</g>
+									<g transform="translate(100.000000, 17.000000)">
+										<rect
+											fill="#BFDBFE"
+											x="0"
+											y="28"
+											width="8"
+											height="19"
+										/>
+										<rect
+											fill="#93C5FD"
+											x="8"
+											y="0"
+											width="8"
+											height="47"
+										/>
+									</g>
+								</g>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+		</g>
+	</svg>
+	<svg
+		v-else-if="value === 'line'"
+		viewBox="0 0 128 64"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+	>
+		<g
+			stroke="none"
+			stroke-width="1"
+			fill="none"
+			fill-rule="evenodd"
+		>
+			<g transform="translate(-71.000000, -268.000000)">
+				<g transform="translate(70.000000, 109.000000)">
+					<g transform="translate(0.000000, 105.000000)">
+						<g transform="translate(0.000000, 52.000000)">
+							<g transform="translate(2.000000, 2.000000)">
+								<path d="M14,0 L114,0 C121.731986,-1.42034288e-15 128,6.2680135 128,14 L128,64 L128,64 L0,64 L0,14 C-9.46895252e-16,6.2680135 6.2680135,1.42034288e-15 14,0 Z" />
+								<path
+									d="M0.5,41.5 C14.9713649,39.0480566 26.812419,37.822085 36.0231625,37.822085 C49.8392777,37.822085 72.8862525,47.0824765 92.3071841,48.2209772 C105.254472,48.9799777 116.985411,46.7396519 127.5,41.5"
+									stroke="#BFDBFE"
+									stroke-linecap="square"
+								/>
+								<path
+									d="M0.5,52.751353 C6.72011181,51.9490165 21.0251398,50.105716 43.4150839,47.2214513 C65.805028,44.3371867 94,33.8179976 128,15.6638841"
+									stroke="#93C5FD"
+									stroke-linecap="square"
+								/>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	props: {
+		value: {
+			type: String,
+			required: true,
+		},
+	},
+});
+</script>
