ec777f603bc87374f645f4eba7f76f9e555d9909
Timur Badretdinov <destinerx@gmail.com>
2021-08-01 16:30:54 +0300

    Options component

---
+++ src/components/Options.vue
@@ -0,0 +1,122 @@
+<template>
+	<RadioGroup
+		:model-value="modelValue"
+		class="options"
+		@update:model-value="setOption"
+	>
+		<RadioGroupLabel class="sr-only">
+			{{ title }}
+		</RadioGroupLabel>
+		<RadioGroupOption
+			v-for="option in options"
+			v-slot="{ active, checked }"
+			:key="option.id"
+			:value="option.id"
+			class="option"
+		>
+			<div
+				class="body"
+				:class="{ active, checked }"
+			>
+				<div class="imagery" />
+				<div class="name">
+					{{ option.name }}
+				</div>
+			</div>
+		</RadioGroupOption>
+	</RadioGroup>
+</template>
+
+<script lang="ts">
+import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue';
+import { PropType, defineComponent } from 'vue';
+
+export interface Option {
+	id: string;
+	name: string;
+}
+
+export default defineComponent({
+	components: {
+		RadioGroup,
+		RadioGroupLabel,
+		RadioGroupOption,
+	},
+	props: {
+		title: {
+			type: String,
+			default: '',
+		},
+		options: {
+			type: Array as PropType<Option[]>,
+			required: true,
+		},
+		modelValue: {
+			type: String,
+			required: true,
+		},
+	},
+	emits: ['update:modelValue'],
+	setup(_props, { emit }) {
+		function setOption(id) {
+			emit('update:modelValue', id);
+		}
+
+		return {
+			setOption,
+		};
+	},
+});
+</script>
+
+<style scoped>
+.options {
+	display: flex;
+}
+
+.option {
+	margin-right: 16px;
+	outline: none;
+}
+
+.body {
+	border: 2px solid #9ca3af;
+	border-radius: 16px;
+}
+
+.body.active {
+	border-color: #1d4ed8;
+}
+
+.body.checked {
+	color: #1d4ed8;
+}
+
+.imagery {
+	width: 128px;
+	height: 64px;
+	border-radius: 14px 14px 0 0;
+	filter: grayscale(0.75);
+}
+
+.body.checked > .imagery {
+	filter: grayscale(0);
+}
+
+.name {
+	font-size: 14px;
+	text-align: center;
+}
+
+.sr-only {
+	position: absolute;
+	width: 1px;
+	height: 1px;
+	margin: -1px;
+	padding: 0;
+	overflow: hidden;
+	clip: rect(0, 0, 0, 0);
+	border-width: 0;
+	white-space: nowrap;
+}
+</style>
