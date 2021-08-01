9d2a33bb6f079bf4d54fbddb94e869335643efc0
Timur Badretdinov <destinerx@gmail.com>
2021-08-01 17:24:43 +0300

    OptionImage component

---
+++ src/components/options/image/OptionImage.vue
@@ -0,0 +1,28 @@
+<template>
+	<OptionImageType
+		v-if="group === 'type'"
+		:value="value"
+	/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import OptionImageType from './OptionImageType.vue';
+
+export default defineComponent({
+	components: {
+		OptionImageType,
+	},
+	props: {
+		group: {
+			type: String,
+			required: true,
+		},
+		value: {
+			type: String,
+			required: true,
+		},
+	},
+});
+</script>
