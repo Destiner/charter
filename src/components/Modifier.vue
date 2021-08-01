e4e6bfeb2984dea4ab2b1d25b82767fb4ae8c780
Timur Badretdinov <destinerx@gmail.com>
2021-08-01 22:57:42 +0300

    Modifier component

---
+++ src/components/Modifier.vue
@@ -0,0 +1,60 @@
+<template>
+	<Switch
+		:model-value="modelValue"
+		class="switch"
+		:class="{ enabled: modelValue }"
+		@update:model-value="setEnabled"
+	>
+		<div>{{ label }}</div>
+	</Switch>
+</template>
+
+<script lang="ts">
+import { Switch } from '@headlessui/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	components: {
+		Switch,
+	},
+	props: {
+		modelValue: {
+			type: Boolean,
+			required: true,
+		},
+		label: {
+			type: String,
+			required: true,
+		},
+	},
+	emits: [ 'update:modelValue' ],
+	setup(_props, { emit }) {
+		function setEnabled(enabled: boolean) {
+			emit('update:modelValue', enabled);
+		}
+
+		return {
+			setEnabled,
+		};
+	},
+});
+</script>
+
+<style scoped>
+.switch {
+	display: inline-flex;
+	position: relative;
+	align-items: center;
+	justify-content: center;
+	width: 128px;
+	padding: 8px 0;
+	border: 2px solid #f3f4f6;
+	border-radius: 16px;
+	background: transparent;
+	font-size: 16px;
+}
+
+.switch.enabled {
+	border-color: #93c5fd;
+}
+</style>
